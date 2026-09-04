e64eaefbb4add5dfe57a3c3e7431ba499519902b
JustJayDev? <bfor97590@gmail.com>
2026-09-04 16:18:36 +0530

    Add service worker for PWA offline support

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'justjaydev-v1';
+const STATIC = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(STATIC))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(k =>
+      Promise.all(k.filter(v => v !== CACHE).map(v => caches.delete(v)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
